3abc4d11df508519d1c8995263df51c66948795d
shaunwhitfield <shaunwhitfield@gmail.com>
2020-01-19 22:34:17 -0500

    Modified templates to pull from database.

--- dex/views.py
+++ dex/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Detail
 
 def detail_list(request):
-    return render(request, 'dex/detail_list.html', {})
+    details = Detail.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'dex/detail_list.html', {'details': details})
