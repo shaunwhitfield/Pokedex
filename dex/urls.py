bdb9a22f2ebeba0fa7c22c029c1edb9c0656c80a
shaunwhitfield <shaunwhitfield@gmail.com>
2020-01-19 23:11:13 -0500

    added view and template for detailed pokemon profiles as well as CSS for the site.

--- dex/urls.py
+++ dex/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.detail_list, name='detail_list'),
+    path('detail/<int:pk>/', views.detail_detail, name='detail_detail'),
 ]
--- dex/views.py
+++ dex/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Detail
 
 def detail_list(request):
     details = Detail.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'dex/detail_list.html', {'details': details})
+
+def detail_detail(request, pk):
+    detail = get_object_or_404(Detail, pk=pk)
+    return render(request, 'dex/detail_detail.html', {'detail': detail})
